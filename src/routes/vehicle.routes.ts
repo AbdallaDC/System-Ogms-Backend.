55abbd13f6be415010fab3dac31f1933b5e309bb
abdiaziz2526 <abdiplayer00000@gmail.com>
2025-05-30 15:42:26 +0300

    vehicle

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -10,10 +10,10 @@ import { protect, restrictTo } from "../middleware/protect";
 
 const router = express.Router();
 
-router.post("/", protect, restrictTo("admin"), createVehicle);
-router.get("/", protect, restrictTo("admin"), getAllVehicles);
-router.get("/:id", protect, restrictTo("admin"), getVehicleById);
-router.put("/:id", protect, restrictTo("admin"), updateVehicle);
-router.delete("/:id", protect, restrictTo("admin"), deleteVehicle);
+router.post("/", protect,createVehicle);
+router.get("/", protect, getAllVehicles);
+router.get("/:id", protect, getVehicleById);
+router.put("/:id", protect, updateVehicle);
+router.delete("/:id", protect,);
 
 export default router;
